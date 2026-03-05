76aa6986aa35de742fb07693705f87b47df80336
YrenoB <ybeltranr@renobo.com.co>
2026-03-05 16:10:08 -0500

    Update Routes.jsx
    Nueva ruta hacia proceso sarlaft

--- FormularioSubastas-main/src/Routes.jsx
+++ FormularioSubastas-main/src/Routes.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes as Switch, Route } from 'react-router-dom';
-import SubsanacionForm from './pages/Form/SubsanacionForm';
+//import SubsanacionForm from './pages/Form/SubsanacionForm';
+import SubsanacionFormSarlaft from './pages/Form/SubsanacionFormSarlaft';
 
 export default function AppRoutes() {
   return (
     <BrowserRouter basename="/formulario">
       <Switch>
-        <Route path="/" element={<SubsanacionForm />} />
+        <Route path="/" element={<SubsanacionFormSarlaft />} />
       </Switch>
     </BrowserRouter>
   );
