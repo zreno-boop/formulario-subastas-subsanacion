c683a12f9ae5987776f12050998df95e718abd6d
zreno-boop <zvelasquezb@renobo.com.co>
2026-09-16 20:07:09 -0500

    arreglos faltantes

--- front/src/pages/Form/FormStep1.jsx
+++ front/src/pages/Form/FormStep1.jsx
@@ -1,10 +1,8 @@
 import Swal from 'sweetalert2';
-//import DatePicker from 'react-datepicker';
 import { useEffect } from 'react';
 import 'react-datepicker/dist/react-datepicker.css';
 import { FaEnvelope, FaPhoneAlt, FaLongArrowAltRight, FaTrash, FaPlus } from 'react-icons/fa';
 import Logos from '../../assets/logosEmpresas.png';
-
 
 export default function FormStep1({ register, errors, watch, setValue, clearErrors, trigger, fieldsIntegrantes, appendIntegrante, removeIntegrante, replaceIntegrantes, next }) {
 
@@ -126,16 +124,13 @@ export default function FormStep1({ register, errors, watch, setValue, clearErro
     const valid = await trigger(fieldsToValidate);
 
     if (!valid) {
-      const firstErrorField = Object.keys(errors)[0];
-      if (firstErrorField) {
-        const element = document.querySelector(
-          `[name="${firstErrorField}"]`
-        );
-        if (element) {
-          element.scrollIntoView({ behavior: "smooth", block: "center" });
-          element.focus();
+      setTimeout(() => {
+        const firstInvalidElement = document.querySelector(".is-invalid");
+        if (firstInvalidElement) {
+          firstInvalidElement.scrollIntoView({ behavior: "smooth", block: "center" });
+          firstInvalidElement.focus();
         }
-      }
+      }, 50);
       return;
     }
     next();
@@ -166,7 +161,7 @@ export default function FormStep1({ register, errors, watch, setValue, clearErro
             <div className="p-2 mt-1 titulos">22 de septiembre de 2026</div>
           </div>
           <div className="col-md-12 col-lg-6">
-          <label className="form-label"><strong>Plazo Límite del Proceso de Inscripción:</strong></label>
+            <label className="form-label"><strong>Plazo Límite del Proceso de Inscripción:</strong></label>
             <div className="p-2 mt-1 titulos">13 de octubre de 2026</div>
           </div>
         </div>
@@ -192,7 +187,7 @@ export default function FormStep1({ register, errors, watch, setValue, clearErro
               className="form-check-input"
               {...register("tipoInscripcion", { required: "Debe seleccionar una opción" })}
             />
-            <label className="form-check-label">Inscripción individual</label>
+            <label className="form-check-label" htmlFor="radioIndividual">Inscripción individual</label>
           </div>
         </div>
         <div className="col-md-12 col-lg-6">
@@ -204,7 +199,7 @@ export default function FormStep1({ register, errors, watch, setValue, clearErro
               className="form-check-input"
               {...register("tipoInscripcion", { required: "Debe seleccionar una opción" })}
             />
-            <label className="form-check-label">Estructura plural</label>
+            <label className="form-check-label" htmlFor="radioGrupal">Estructura plural</label>
           </div>
           {errors.tipoInscripcion && (
             <p className="text-danger small">{errors.tipoInscripcion.message}</p>
@@ -369,11 +364,9 @@ export default function FormStep1({ register, errors, watch, setValue, clearErro
                   pattern: { value: telefonoPattern, message: 'Formato teléfono inválido sin espacios, sólo números. (ej: celular inicia con 3 o fijo inicia por 6 o 0)' },
                 })}
               />
-              <div className="invalid-feedback">{errors.telefono?.message}
-              </div>
+              <div className="invalid-feedback">{errors.telefono?.message}</div>
             </div>
           </div>
-
         </div>
       )}
 
@@ -398,7 +391,6 @@ export default function FormStep1({ register, errors, watch, setValue, clearErro
                 </div>
 
                 <div className="row gx-3 gy-2">
-
                   <div className="col-md-12">
                     <label className="form-label">Correo Electrónico (para notificaciones oficiales) *</label>
                     <div className="input-group">
@@ -424,7 +416,7 @@ export default function FormStep1({ register, errors, watch, setValue, clearErro
                         required: 'Nombre o razón social obligatorio',
                         minLength: { value: 3, message: 'Mínimo 3 caracteres' },
                         pattern: {
-                          value: /^(?=.*[A-Za-zÁÉÍÓÚÜÑáéíóúüñ])[A-Za-zÁÉÍÓÚÜÑáéíóúüñ0-9.,\-\s&#%+/()'"]{3,150}$/,
+                          value: /^(?=.*[A-Za-zÁÉÍÓÚÜÑáéíóúüñ])[A-Za-zÁÉÍÓÚÜÑáéíóúüñ0-9.,\s&#%+/()'"-]{3,150}$/,
                           message: 'Sólo se permiten letras, números, espacios, puntos guiones y &',
                         },
                         validate: (v) =>
@@ -551,8 +543,7 @@ export default function FormStep1({ register, errors, watch, setValue, clearErro
                           pattern: { value: telefonoPattern, message: 'Formato teléfono inválido sin espacios, sólo números. (ej: celular inicia con 3 o fijo inicia por 6 o 0)' },
                         })}
                       />
-                      <div className="invalid-feedback">{errors.integrantes?.[index]?.telefono?.message}
-                      </div>
+                      <div className="invalid-feedback">{errors.integrantes?.[index]?.telefono?.message}</div>
                     </div>
                   </div>
 
@@ -563,7 +554,6 @@ export default function FormStep1({ register, errors, watch, setValue, clearErro
                       type="checkbox"
                       {...register(`integrantes.${index}.autorizacionOrgano`, {
                         required: 'Debe aceptar los términos y condiciones'
-
                       })}
                     />
                     <label className="form-check-label ms-2" htmlFor={`autorizacionOrgano-${index}`}>
@@ -595,7 +585,6 @@ export default function FormStep1({ register, errors, watch, setValue, clearErro
                     />
                     <div className="invalid-feedback">{errors.integrantes?.[index]?.autorizacionOrganoDocumento?.message}</div>
                   </div>
-
                 </div>
               </div>
             </div>
@@ -628,8 +617,6 @@ export default function FormStep1({ register, errors, watch, setValue, clearErro
           Siguiente <FaLongArrowAltRight />
         </button>
       </div>
-
-
     </>
   );
 }
